13caa2400070021773658a7179f7976da4832268
Jarilton <jariltonpereira02@hotmail.com>
2022-08-16 01:09:22 -0300

    Proporções de medidas no header do usuário

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreting,
+    UserName
+} from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/59670578?v=4' }}
+                        />
+                        <User>
+                            <UserGreting>Olá,</UserGreting>
+                            <UserName>Jamal</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
