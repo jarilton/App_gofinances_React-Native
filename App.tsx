d7b982622204736f662d55d8eb6fbed1d9355d95
Jarilton <jariltonpereira02@hotmail.com>
2022-08-15 23:10:38 -0300

    criando Componente com styled components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Welcome title='Welcome! Project Go Finances' />
-    </View>
+    <Dashboard />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
