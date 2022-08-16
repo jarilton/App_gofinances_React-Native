b805183cfb3f2d7847dd9f556d853c45e7a5cf17
Jarilton <jariltonpereira02@hotmail.com>
2022-08-15 22:51:21 -0300

    Adicionando typescript

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { View, Text} from 'react-native';
+
+interface Props {
+    title: string;
+}
+
+export function Welcome({ title }:Props) {
+    return (
+        <View>
+            <Text>
+                { title }
+            </Text>
+        </View>
+    );
+}
