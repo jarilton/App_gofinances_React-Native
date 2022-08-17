97e740e22e8ee7d9eb0a37039e1d168311256d72
Jarilton <jariltonpereira02@hotmail.com>
2022-08-17 01:33:35 -0300

    Tipando e acessando props no styled-components

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -9,16 +9,38 @@ import {
   LastTransaction,
 } from "./styles";
 
-export function Cards() {
+interface Props {
+  type: "up" | "down" | "total";
+  title: string;
+  amount: string;
+  lastTransaction: string;
+}
+
+const icon = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+  total: "dollar-sign",
+}
+
+export function Cards({ type, title, amount, lastTransaction }: Props) {
   return (
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>Entrada</Title>
-        <Icon name="arrow-up-circle" />
+        <Title type={type}>
+          {title}
+        </Title>
+        <Icon
+          name={icon[type]}
+          type={type}
+        />
       </Header>
 
-      <Amount>R$ 17.400,00</Amount>
-      <LastTransaction>Última entrada dia 13 de abril</LastTransaction>
+      <Amount type={type}>
+        {amount}
+      </Amount>
+      <LastTransaction type={type}>
+        {lastTransaction}
+      </LastTransaction>
     </Container>
   );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -35,9 +35,24 @@ export function Dashboard() {
       </Header>
 
       <CardsGroup>
-        <Cards />
-        <Cards />
-        <Cards />
+        <Cards
+          type="up"
+          title="Entrada"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <Cards
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <Cards
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
       </CardsGroup>
     </Container>
   );
