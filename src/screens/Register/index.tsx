1ff9c7769af58563705dded3cef3dc4c4b298a4b
Jarilton <jariltonpereira02@hotmail.com>
2022-08-18 23:22:29 -0300

    Componente de botão

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Button } from "../../components/Form/Button";
 import { Input } from "../../components/Form/Input";
-import { Container, Header, Title, Form } from "./styles";
+import { Container, Header, Title, Form, Fields } from "./styles";
 
 export function Register() {
   return (
@@ -10,8 +11,12 @@ export function Register() {
       </Header>
 
       <Form>
-        <Input placeholder="Nome" />
-        <Input placeholder="Preço" />
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+        </Fields>
+
+        <Button title="Enviar" />
       </Form>
     </Container>
   );
