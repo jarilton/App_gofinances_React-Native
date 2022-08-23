309e32e21b23d06c6ad253f66d2b4c7160906fa2
Jarilton <jariltonpereira02@hotmail.com>
2022-08-22 22:41:02 -0300

    Adicionando navegação com a tab navigation

--- App.tsx
+++ App.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_700Bold
-} from '@expo-google-fonts/poppins'
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
-import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import theme from "./src/global/styles/theme";
 
+import { NavigationContainer } from "@react-navigation/native";
+
+import { AppRoutes } from "./src/routes/app.routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold
+    Poppins_700Bold,
   });
 
-  if(!fontsLoaded) {
-    return <AppLoading />
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
-
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+import { Dashboard } from "../screens/Dashboard";
+import { Register } from "../screens/Register";
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="Listagem" component={Dashboard} />
+      <Screen name="Cadastro" component={Register} />
+      <Screen name="Resumo" component={Register} />
+    </Navigator>
+  );
+}
